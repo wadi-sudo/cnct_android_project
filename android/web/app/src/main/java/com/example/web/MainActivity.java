10ebee8c42ea0bc0a30d2ea69633c3c35000b7eb
wadi-sudo <boughanmi.wadi@gmail.com>
2022-04-12 10:37:47 +0200

    android app with samsam

---
+++ android/web/app/src/main/java/com/example/web/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.web;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView=(WebView) findViewById(R.id.webview);
+        mywebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings=mywebView.getSettings();
+        mywebView.getSettings().setGeolocationEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.getSettings().setGeolocationDatabasePath(getFilesDir().getPath());
+        // Below required for geolocation
+        mywebView.setWebViewClient(new WebViewClient());
+        mywebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        mywebView.loadUrl("https://wadi-sudo.github.io/");
+
+    }
+    public class mywebClient extends WebViewClient{
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon){
+            super.onPageStarted(view,url,favicon);
+        }
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(mywebView.canGoBack()) {
+            mywebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
